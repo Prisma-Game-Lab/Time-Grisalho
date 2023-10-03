93f187ae9a2728620d513d37862ed768816d0e66
GratzTheJail <guigagratz@hotmail.com>
2023-10-02 21:32:52 -0300

    Mecanica de foto (quase pronta)
    - Ao pressionar "E" ativa/desativa a câmera
    - Ao clicar com botão esquerdo com câmera tira foto
    - Se o "Alvo" (circulo vermelho) ta em contato com a parte verde da camera dá debug log que a foto foi tirada e volta pro menu, se o alvo tiver fora do verde dá um debug log feedback negativo
    
    o que falta ainda:
    - ao invés do alvo estar em contato com a parte verde, o alvo deve estar 100% dentro da área geral da foto
    - criar botões na ui pra abrir/fechar camera e pra tirar foto

---
+++ Assets/Scripts/InputCamera.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class InputCamera : MonoBehaviour
+{
+    Vector3 posMouse;
+    public GameObject camera_player, areaFoto, alvo; // camera que o player puxa pra tirar foto
+    private Vector2 topLeft, bottomRight;
+
+    private void cameraSegueMouse() 
+    {
+        posMouse = Input.mousePosition;
+        posMouse.z = Camera.main.nearClipPlane;
+        posMouse = Camera.main.ScreenToWorldPoint(posMouse);
+        camera_player.transform.position = posMouse;
+    }
+
+    private IEnumerator waiter()
+    {
+        yield return new WaitForSecondsRealtime(5);
+    }
+
+    private void tiraFoto()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            topLeft = areaFoto.GetComponent<BoxCollider2D>().bounds.min;
+            bottomRight = areaFoto.GetComponent<BoxCollider2D>().bounds.max;
+            if (Physics2D.OverlapArea(topLeft, bottomRight, LayerMask.GetMask("Alvo")))
+            {
+                Debug.Log("tirou foto certo!");
+                StartCoroutine(waiter());
+                SceneManager.LoadScene("MainMenu");
+            } 
+            else
+            {
+                Debug.Log("tirou foto errado!");
+            }
+            camera_player.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        camera_player.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (!camera_player.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                camera_player.SetActive(true);
+            }
+        } else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                camera_player.SetActive(false);
+            }
+            cameraSegueMouse();
+            tiraFoto();
+        }
+    }
+}
