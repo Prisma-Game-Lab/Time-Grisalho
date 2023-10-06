ae018a7a86ede35e6c03e63d40ca4b09f2785ba5
João Marcello <131413084+jmal93@users.noreply.github.com>
2023-10-06 12:58:52 -0300

    Conserto bug

--- Assets/Scripts/ConcluiFase.cs
+++ Assets/Scripts/ConcluiFase.cs
@@ -7,8 +7,15 @@ public class ConcluiFase : MonoBehaviour
 {
     [SerializeField]
     private NumDeFases numDeFases;
+    [SerializeField]
+    private ScenesManager gerenciadorDeCena;
     private void Start()
     {
+        gerenciadorDeCena = FindAnyObjectByType<ScenesManager>();
+    }
+    public void AumentaNumeroDeFases()
+    {
         numDeFases.numeroDeFases++;
+        gerenciadorDeCena.GoToScene();
     }
 }
