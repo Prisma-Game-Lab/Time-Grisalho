c40b812ee8ee2105112231d880b31bd2637c8a92
João Marcello <131413084+jmal93@users.noreply.github.com>
2023-10-06 15:11:17 -0300

    timer de conclusão de fase

--- Assets/Scripts/ConcluiFase.cs
+++ Assets/Scripts/ConcluiFase.cs
@@ -9,13 +9,21 @@ public class ConcluiFase : MonoBehaviour
     private NumDeFases numDeFases;
     [SerializeField]
     private ScenesManager gerenciadorDeCena;
+    private IEnumerator coroutine;
     private void Start()
     {
         gerenciadorDeCena = FindAnyObjectByType<ScenesManager>();
+        coroutine = Espera();
     }
     public void AumentaNumeroDeFases()
     {
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Espera()
+    {
         numDeFases.numeroDeFases++;
+        yield return new WaitForSeconds(3.0f);
         gerenciadorDeCena.GoToScene();
     }
 }
