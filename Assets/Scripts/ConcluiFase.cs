0f0b3a3b18747b23274541361b64f3049e659a05
João Marcello <131413084+jmal93@users.noreply.github.com>
2023-10-06 16:14:23 -0300

    Câmera aparece mas ainda não tira foto

--- Assets/Scripts/ConcluiFase.cs
+++ Assets/Scripts/ConcluiFase.cs
@@ -9,6 +9,8 @@ public class ConcluiFase : MonoBehaviour
     private NumDeFases numDeFases;
     [SerializeField]
     private ScenesManager gerenciadorDeCena;
+    [SerializeField]
+    private float timer;
     private IEnumerator coroutine;
     private void Start()
     {
@@ -23,7 +25,7 @@ public class ConcluiFase : MonoBehaviour
     private IEnumerator Espera()
     {
         numDeFases.numeroDeFases++;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(timer);
         gerenciadorDeCena.GoToScene();
     }
 }
